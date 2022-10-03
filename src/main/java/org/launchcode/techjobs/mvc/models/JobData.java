d20cbe6bd996fec344bfc362b944244d2363200d
bree-jeune <bree.j.32@gmail.com>
2022-10-03 12:12:28 -0700

    completed assignment with tests passing

--- src/main/java/org/launchcode/techjobs/mvc/models/JobData.java
+++ src/main/java/org/launchcode/techjobs/mvc/models/JobData.java
@@ -7,10 +7,7 @@ import org.launchcode.techjobs.mvc.NameSorter;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,42 +25,26 @@ public class JobData {
     private static ArrayList<PositionType> allPositionTypes = new ArrayList<>();
     private static ArrayList<CoreCompetency> allCoreCompetency = new ArrayList<>();
 
-    /**
-     * Fetch list of all job objects from loaded data,
-     * without duplicates, then return a copy.
-     */
 
     public static ArrayList<Job> findAll() {
 
-        // load data, if not already loaded
         loadData();
 
-        // Bonus mission; normal version returns allJobs
         return new ArrayList<>(allJobs);
     }
 
-    /**
-     * Returns the results of searching the Jobs data by field and search term.
-     *
-     * For example, searching for employer "Enterprise" will include results
-     * with "Enterprise Holdings, Inc".
-     *
-     * @param column Job field that should be searched.
-     * @param value Value of the field to search for.
-     * @return List of all jobs matching the criteria.
-     */
+
     public static ArrayList<Job> findByColumnAndValue(String column, String value) {
 
-        // load data, if not already loaded
         loadData();
 
         ArrayList<Job> jobs = new ArrayList<>();
 
-        if (value.toLowerCase().equals("all")){
+        if (value.equalsIgnoreCase("all")) {
             return findAll();
         }
 
-        if (column.equals("all")){
+        if (column.equals("all")) {
             jobs = findByValue(value);
             return jobs;
         }
@@ -79,15 +60,15 @@ public class JobData {
         return jobs;
     }
 
-    public static String getFieldValue(Job job, String fieldName){
+    public static String getFieldValue(Job job, String fieldName) {
         String theValue;
-        if (fieldName.equals("name")){
+        if (fieldName.equals("name")) {
             theValue = job.getName();
-        } else if (fieldName.equals("employer")){
+        } else if (fieldName.equals("employer")) {
             theValue = job.getEmployer().toString();
-        } else if (fieldName.equals("location")){
+        } else if (fieldName.equals("location")) {
             theValue = job.getLocation().toString();
-        } else if (fieldName.equals("positionType")){
+        } else if (fieldName.equals("positionType")) {
             theValue = job.getPositionType().toString();
         } else {
             theValue = job.getCoreCompetency().toString();
@@ -95,18 +76,15 @@ public class JobData {
 
         return theValue;
     }
-    /**
-     * Search all Job fields for the given term.
-     *
-     * @param value The search term to look for.
-     * @return      List of all jobs with at least one field containing the value.
-     */
+
     public static ArrayList<Job> findByValue(String value) {
 
-        // load data, if not already loaded
         loadData();
 
         ArrayList<Job> jobs = new ArrayList<>();
+
+
+
 
         for (Job job : allJobs) {
 
@@ -122,33 +100,31 @@ public class JobData {
                 jobs.add(job);
             }
 
+
         }
 
         return jobs;
     }
 
-    private static Object findExistingObject(ArrayList list, String value){
-        for (Object item : list){
-            if (item.toString().toLowerCase().equals(value.toLowerCase())){
+    private static Object findExistingObject(ArrayList list, String value) {
+        for (Object item : list) {
+            if (item.toString().equalsIgnoreCase(value)) {
                 return item;
             }
         }
         return null;
     }
 
-    /**
-     * Read in data from a CSV file and store it in an ArrayList of Job objects.
-     */
+
     private static void loadData() {
 
-        // Only load data once
         if (isDataLoaded) {
             return;
         }
 
         try {
 
-            // Open the CSV file and set up pull out column header info and records
+
             Resource resource = new ClassPathResource(DATA_FILE);
             InputStream is = resource.getInputStream();
             Reader reader = new InputStreamReader(is);
@@ -159,7 +135,7 @@ public class JobData {
 
             allJobs = new ArrayList<>();
 
-            // Put the records into a more friendly format
+
             for (CSVRecord record : records) {
 
                 String aName = record.get(0);
@@ -173,22 +149,22 @@ public class JobData {
                 PositionType newPosition = (PositionType) findExistingObject(allPositionTypes, aPosition);
                 CoreCompetency newSkill = (CoreCompetency) findExistingObject(allCoreCompetency, aSkill);
 
-                if (newEmployer == null){
+                if (newEmployer == null) {
                     newEmployer = new Employer(anEmployer);
                     allEmployers.add(newEmployer);
                 }
 
-                if (newLocation == null){
+                if (newLocation == null) {
                     newLocation = new Location(aLocation);
                     allLocations.add(newLocation);
                 }
 
-                if (newSkill == null){
+                if (newSkill == null) {
                     newSkill = new CoreCompetency(aSkill);
                     allCoreCompetency.add(newSkill);
                 }
 
-                if (newPosition == null){
+                if (newPosition == null) {
                     newPosition = new PositionType(aPosition);
                     allPositionTypes.add(newPosition);
                 }
@@ -197,7 +173,7 @@ public class JobData {
 
                 allJobs.add(newJob);
             }
-            // flag the data as loaded, so we don't do it twice
+
             isDataLoaded = true;
 
         } catch (IOException e) {
@@ -231,4 +207,3 @@ public class JobData {
     }
 
 }
-
